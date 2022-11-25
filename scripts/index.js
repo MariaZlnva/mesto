a62e47edd124a3874b478f5abe0b2dbfb13c4dd0
Maria Zelenova <zma611@yandex.ru>
2022-11-25 18:28:31 +0400

    add popup picture big

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,36 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
+
 // popups elements
 const popupEditProfile = document.querySelector(".popup_edit-profile");
 const popupAddCard = document.querySelector(".popup_add-cards");
-const popunElementPicture = document.querySelector(".popup_big-picture");
+// const popunElementPicture = document.querySelector(".popup_big-picture");
 
 // popups open buttons
 const popupButtonOpenEdit = document.querySelector(".profile__edit");
