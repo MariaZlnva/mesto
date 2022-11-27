7be962c929e45e701f798b1aae08fe06d0cd6c67
Maria Zelenova <zma611@yandex.ru>
2022-11-28 00:45:41 +0400

    add feature click like

--- scripts/index.js
+++ scripts/index.js
@@ -25,58 +25,21 @@ const initialCards = [
   }
 ]; 
 
-
-const cardPlace = document.querySelector(".places");
-const cardTemplate = document.querySelector("#card-template").content.querySelector(".places__item");
-
-
-
-// функция создания элемента (у функции одно действие должно быть - здесь только создание)
-function createElement (card){
-  const cardElement = cardTemplate.cloneNode(true);
-  const likeButton = cardElement.querySelector(".places__like");
-  const cardName = cardElement.querySelector(".places__title");
-  const cardImage = cardElement.querySelector(".places__image");
-  cardName.textContent = card.name;
-  cardImage.src = card.link;
-    
-  likeButton.addEventListener('click', handleLikeButton);
-
-  return cardElement;
-  }
-
-// обработчик like
-const handleLikeButton = (evt) => {
-  evt.target.classList.toggle("places_like-active");
-}
-
-console.log(handleLikeButton);
-// функция которая получает в кач-ве аргумента card и создает ее (и для создания карточки и для добавления)
-const renderCard = function(card){
-  const element = createElement(card);
-  cardPlace.append(element);
-  
-}
-
-// перебираем массив, в кач-ве аргум функция, которая вызывается на каждый элемент массива
-initialCards.forEach(function(card){
-  renderCard(card);
-  
-  })
-
-
 // popups elements
 const popupEditProfile = document.querySelector(".popup_edit-profile");
 const popupAddCard = document.querySelector(".popup_add-cards");
-// const popunElementPicture = document.querySelector(".popup_big-picture");
+const popunPicture = document.querySelector(".popup_big-picture");
 
 // popups open buttons
 const popupButtonOpenEdit = document.querySelector(".profile__edit");
 const popupButtonAddCard = document.querySelector(".profile__add");
+const popupClickImage = document.querySelector(".popup__image-big");
 
 // popup close buttons
 const popupButtonCloseEdit = popupEditProfile.querySelector(".popup__close");
 const popupButtonCloseAdd = popupAddCard.querySelector(".popup__close");
+const popupButtonCloseImage = popunPicture.querySelector(".popup__close");
+
 
 const formEditProfile = popupEditProfile.querySelector(".popup__inputs");
 const formAddCard = popupAddCard.querySelector(".popup__inputs");
@@ -88,6 +51,54 @@ const profileInfo = document.querySelector(".profile__subtitle");
 
 const nameInput = formEditProfile.querySelector(".popup__input_name");
 const infoInput = formEditProfile.querySelector(".popup__input_info");
+
+const cardPlace = document.querySelector(".places");
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".places__item");
+
+
+
+// функция создания элемента (у функции одно действие должно быть - здесь только создание)
+function createElement (card){
+  const cardElement = cardTemplate.cloneNode(true);
+  const likeButton = cardElement.querySelector(".places__like");
+  const deleteButton = cardElement.querySelector(".places__delete");
+  const cardName = cardElement.querySelector(".places__title");
+  const cardImage = cardElement.querySelector(".places__image");
+  cardName.textContent = card.name;
+  cardImage.src = card.link;
+    
+  likeButton.addEventListener('click', handlerLikeButton);
+  deleteButton.addEventListener('click', handlerDeleteButton);
+  // popupClickImage.addEventListener('click', handlerImageClick);
+
+  return cardElement;
+  }
+
+
+// функция обработчик like
+const handlerLikeButton = (evt) => {
+  evt.target.classList.toggle("places_like-active");
+}
+
+// функция обработчик delete
+const handlerDeleteButton = (evt) => {
+  evt.target.closest(".places__item").remove();
+}
+
+// const handlerImageClick = function(evt){
+//   ev
+// }
+
+// функция которая получает в кач-ве аргумента card, создает карточку и помещает в верстку
+const renderCard = function(card){
+  const element = createElement(card);
+  cardPlace.prepend(element);
+  }
+
+// перебираем массив, в кач-ве аргум функция, которая вызывается на каждый элемент массива
+initialCards.forEach(function(card){
+  renderCard(card);
+  })
 
 // function open popup
 const popupOpen = function (evt) {
@@ -111,27 +122,24 @@ function formSubmitHandler(evt) {
   popupClose(popupEditProfile);
   }
 
-formEditProfile.addEventListener("submit", formSubmitHandler);
 
 
-
-// обработчки отправки формы создать
-function handleFormSubmit (evt) {
+// функция обработчик события формы добавления карточки
+function handlerFormSubmit (evt) {
   evt.preventDefault(); 
- const cardElement = {
-  name: formInputName.value,
-  link: formInputLink.value,
- }
+    const cardElement = {
+      name: formInputName.value,
+      link: formInputLink.value,
+    }
   renderCard(cardElement);
-
   popupClose(popupAddCard);
 }
 
-formAddCard.addEventListener('submit', handleFormSubmit); 
 
 
 
-// обработчики событий открытия popups
+
+// функции слушатели событий открытия/закрытия попапов
 popupButtonOpenEdit.addEventListener("click", function() {
   popupOpen(popupEditProfile)
 });
@@ -139,7 +147,6 @@ popupButtonAddCard.addEventListener("click", function() {
   popupOpen(popupAddCard)
 });
 
-// обработчики событий закрытия popups
 popupButtonCloseEdit.addEventListener("click", function() {
   popupClose(popupEditProfile)
 });
@@ -147,5 +154,11 @@ popupButtonCloseAdd.addEventListener("click", function() {
   popupClose(popupAddCard)
 });
 
+
+
+
+formAddCard.addEventListener('submit', handlerFormSubmit); 
+
+formEditProfile.addEventListener("submit", formSubmitHandler);
 
 
