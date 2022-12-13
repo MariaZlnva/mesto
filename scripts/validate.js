96fe32114d45b6fc8f663e51582757d270881aa0
Maria Zelenova <zma611@yandex.ru>
2022-12-13 17:48:48 +0400

    доб.передача config в функции кнопка частично активная

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,62 +1,79 @@
-const config = {
+
+const hideError = (inputSelector, error, config) => {
+  inputSelector.classList.remove(config.inputErrorClass);
+  error.classList.remove(config.errorClass);
+  error.textContent = "";
+};
+
+const showError = (inputSelector, error, config) => {
+  inputSelector.classList.add(config.inputErrorClass);
+  error.classList.add(config.errorClass);
+  error.textContent = inputSelector.validationMessage;
+};
+
+const checkValidityInput = (inputSelector, config) => {
+  const error = document.querySelector(`#${inputSelector.id}-error`);
+  if (inputSelector.validity.valid) {
+    hideError(inputSelector, error, config);
+  } else {
+    showError(inputSelector, error, config);
+  }
+};
+
+const isValidForm = (inputs) => {
+  return inputs.every(inputSelector => {
+  return inputSelector.validity.valid 
+}
+  )};
+const toggleButtonState = (inputs, buttonSubmit, config) => {
+     if (isValidForm(inputs)) {
+    console.log("кнопка должна бытьактивная")
+    buttonSubmit.classList.remove(config.inactiveButtonClass);
+    buttonSubmit.removeAttribute("disabled");
+  } else {
+    console.log("кнопка должна быть неактивная")
+    buttonSubmit.classList.add(config.inactiveButtonClass);
+    buttonSubmit.disabled  = true;
+  }
+}
+
+const setEventListeners = (formSelector, config) => {
+  
+  const inputs = Array.from(document.querySelectorAll(config.inputSelector));
+  const buttonSubmit = formSelector.querySelector(config.submitButtonSelector);
+ 
+  toggleButtonState(inputs, buttonSubmit, config);
+
+  inputs.forEach((inputSelector) => {
+        inputSelector.addEventListener("input", () => {
+          
+          checkValidityInput(inputSelector, config);
+          toggleButtonState(inputs, buttonSubmit, config);
+          
+        });
+  });
+}
+
+const enableValidation = (config) => {
+  const forms = Array.from(document.querySelectorAll(config.formSelector));
+    
+  forms.forEach((formSelector) => {
+    formSelector.addEventListener("submit", (evt) => {
+          evt.preventDefault();
+        });
+        
+    setEventListeners(formSelector, config);
+    
+  });
+}
+enableValidation({
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
-};
-
-const forms = Array.from(document.querySelectorAll(config.formSelector));
-
-const hideError = (inputSelector, error) => {
-  inputSelector.classList.remove(config.inputErrorClass);
-  error.classList.remove(config.errorClass);
-  error.textContent = "";
-};
-
-const showError = (inputSelector, error) => {
-  inputSelector.classList.add(config.inputErrorClass);
-  error.classList.add(config.errorClass);
-  error.textContent = inputSelector.validationMessage;
-};
-
-const checkValidityInput = (inputSelector, error) => {
-  if (inputSelector.validity.valid) {
-    hideError(inputSelector, error);
-  } else {
-    showError(inputSelector, error);
-  }
-};
-
-const toggleButtonState = (inputs, inputSelector, buttonSubmit) => {
-  const isValidForm = inputs.every(
-    (inputSelector) => inputSelector.validity.valid
-  );
-  if (isValidForm) {
-    buttonSubmit.classList.remove(config.inactiveButtonClass);
-    buttonSubmit.removeAttribute("disabled");
-  } else {
-    buttonSubmit.classList.add(config.inactiveButtonClass);
-    buttonSubmit.setAttribute("disabled", true);
-  }
-}
-
-
-forms.forEach((formSelector) => {
-  const inputs = Array.from(formSelector.querySelectorAll(config.inputSelector));
-  const buttonSubmit = formSelector.querySelector(config.submitButtonSelector);
-
-  formSelector.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-  });
-  inputs.forEach((inputSelector) => {
-    inputSelector.addEventListener("input", () => {
-      const error = document.querySelector(`#${inputSelector.id}-error`);
-
-      checkValidityInput(inputSelector, error);
-      toggleButtonState(inputs, inputSelector, buttonSubmit);
-      
-    });
-  });
 });
+
+
+
